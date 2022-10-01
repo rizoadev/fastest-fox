44e80c6b206b18f5f2092a496cc8c802016208c4
Riza Masykur <riza@ikamai.com>
2022-10-02 02:03:12 +0700

    j2

--- .github/actions/backup/app/run.sh
+++ .github/actions/backup/app/run.sh
@@ -5,4 +5,7 @@ cd /home
 # PGPASSWORD=postgres pg_dump -Upostgres -h194.233.88.100 --port 5401 -F c ikamai_hive > /home/ikamai_hive.sql
 ls
 
+mkdir -p /github/home/.config/rclone/
+cp /app/rclone.conf /github/home/.config/rclone/rclone.conf
+
 rclone sync /home idrive:/lume/jclone --progress
