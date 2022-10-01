118ab5f8263034370c8e465fd8f8061568332ab4
Riza Masykur <riza@ikamai.com>
2022-10-02 02:18:45 +0700

    asuik

--- .github/actions/backup/app/run.sh
+++ .github/actions/backup/app/run.sh
@@ -11,7 +11,7 @@ echo $today
 echo "running..."
 mkdir -p /home/backups/$today/
 
-mysqldump -uaimigroup -h194.233.86.165 -pzw43uyaoYTJHKGTYFhyuit3435t9w4n8yt8 aimi_dev > /home/backups/$today/aimi_dev.sql
+# mysqldump -uaimigroup -h194.233.86.165 -pzw43uyaoYTJHKGTYFhyuit3435t9w4n8yt8 aimi_dev > /home/backups/$today/aimi_dev.sql
 
 PGPASSWORD=postgres pg_dump -Upostgres -h194.233.88.100 --port 5401 -F c ikamai_hive > /home/backups/$today/ikamai_hive.sql
 
