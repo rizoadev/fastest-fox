2ab186e94012543368af08c9e00a7efc95649871
Riza Masykur <riza@ikamai.com>
2022-10-02 01:54:49 +0700

    sempak

---
+++ .github/actions/backup/app/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "running..."
+cd /home
+PGPASSWORD=postgres pg_dump -Upostgres -h194.233.88.100 --port 5401 -F c ikamai_hive > /home/ikamai_hive.sql
+ls
+
+rclone sync /home idrive:/lume/jclone --progress
